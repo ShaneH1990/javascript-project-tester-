6450bc2dbcfbe4507530e3532aade5ddc30e24d0
ShaneH1990 <shaneharraghy@live.ie>
2022-08-08 21:44:52 +0000

    addedd list iems

---
+++ function.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+
+    let answer1 = document.getElementById('mi');
+    let answer2 = document.getElementById('tg');
+    let answer3 = document.getElementById('jr');
+    let answer4 = document.getElementById('rd');
+    let answer5 = document.getElementById('tc');
+    let answer6 = document.getElementById('ce');
+
+    answer1.addEventListener("click", function (){ alert("you selected Mission Impossible");});
+    answer2.addEventListener("click", function (){ alert("you selected Top Gun");});
+    answer3.addEventListener("click", function (){ alert("you selected Jack Reacher");});
+    answer4.addEventListener("click", function (){ alert("you selected Robert Downey Jr");});
+    answer5.addEventListener("click", function (){ alert("you selected Tom Cruise");});
+    answer6.addEventListener("click", function (){ alert("you selected Chris evans");});
+})
