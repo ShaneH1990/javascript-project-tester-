fd2e185ac31dbf80f4417646336bb53308a357a1
ShaneH1990 <shaneharraghy@live.ie>
2022-09-15 22:35:58 +0000

    figured out how to get my own functions to work inside the preset function for my list items!

--- function.js
+++ function.js
@@ -7,11 +7,17 @@ document.addEventListener("DOMContentLoaded", function(){
     let answer4 = document.getElementById('rd');
     let answer5 = document.getElementById('tc');
     let answer6 = document.getElementById('ce');
-
-    answer1.addEventListener("click", function (){ alert("you selected Mission Impossible");});
+    //I can put my own declared function(answered) inside of the function for answer1 and it works perfectly!
+    answer1.addEventListener("click", function (){ answered();});
     answer2.addEventListener("click", function (){ alert("you selected Top Gun");});
     answer3.addEventListener("click", function (){ alert("you selected Jack Reacher");});
     answer4.addEventListener("click", function (){ alert("you selected Robert Downey Jr");});
     answer5.addEventListener("click", function (){ alert("you selected Tom Cruise");});
     answer6.addEventListener("click", function (){ alert("you selected Chris evans");});
+
+    function answered(){
+        alert("you selected Mission Impossible");
+
+
+    }
 })
